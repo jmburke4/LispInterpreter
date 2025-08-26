280389f6e285b21dd7d0a77fa7619f5e6352a7de
ban <jmburke4@crimson.ua.edu>
2025-08-25 22:30:40 -0500

    Added gll library from Github for lgeneric linked lists. main.c has examples of storing int's and strings

--- src/main.c
+++ src/main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
-#include "my_module.h"
+// #include "util.h"
+#include "gll.h"
+// #include "lexer.h"
+// #include "parser.h"
+
+gll_t* list;
 
 int main() {
-    int result = add(5, 3);
-    printf("Result: %d\n", result);
+    // This is the Windows relative path
+    // can also use absolute path "R:\\LispInterpreter\\tests\\s1.txt"
+    //int result = util_readFile(".\\tests\\s1.txt", 256, util_printBuffer);
+
+    list = gll_init();
+
+    char* a = "A";
+    char* b = "B";
+    char* c = "str";
+
+    // int a = 1;
+    // int b = 2;
+    // int c = 3;
+
+    gll_pushBack(list, &a);
+    gll_pushBack(list, &b);
+    gll_pushBack(list, &c);
+
+    gll_node_t* t = list->first;
+
+    while (t != NULL){
+        // int* t_int = (int*)t->data;
+        // fprintf(stdout, "%d\n", *t_int);
+
+        char** t_char = (char**)t->data;
+        fprintf(stdout, "%s\n", *t_char);
+
+        t = t->next;
+    }
+
     return 0;
 }
