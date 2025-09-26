1dd542873cbbf55ca36826189ff30daec3a41c4c
ban <jmburke4@crimson.ua.edu>
2025-09-26 10:44:57 -0500

    Add runLine method to main.c

--- src/main.c
+++ src/main.c
@@ -7,6 +7,31 @@
 
 #define LINE_BUFFER_SIZE 256
 
+void runLine(char _buffer[], TokenList* _tokens, int _repl){
+    // fgets includes the newline character if there's space.
+    // To remove it, you can find and replace it with a null terminator.
+    size_t len = strlen(_buffer);
+    if (len > 0 && _buffer[len-1] == '\n') {
+        _buffer[len-1] = '\0';
+    }
+    
+    lexer_scanTokens(_buffer, _tokens);
+    if (_tokens->size > 0){
+        if (_repl == 0) printf("\n>%s\n", _buffer);
+        
+        lexer_printTokens(_tokens);
+        parser_setList(_tokens->first);
+        SExpression *exp = parser_parseExpression();
+        lexer_clearTokenList(_tokens);
+        parser_print(exp);
+        parser_clearExpression(exp);
+        
+        // Flush output stream for debugging
+        printf("\n");
+        fflush(stdout);
+    }
+}
+
 int main(int argc, char *argv[]) {
     int result = 0;
     TokenList *tokens = lexer_initTokenList();
@@ -20,28 +45,7 @@ int main(int argc, char *argv[]) {
         }
 
         while (fgets(buffer, sizeof(buffer), fptr) != NULL){
-            // fgets includes the newline character if there's space.
-            // To remove it, you can find and replace it with a null terminator.
-            size_t len = strlen(buffer);
-            if (len > 0 && buffer[len-1] == '\n') {
-                buffer[len-1] = '\0';
-            }
-
-            lexer_scanTokens(buffer, tokens);
-            if (tokens->size > 0){
-                printf("\n>%s\n", buffer);
-                
-                lexer_printTokens(tokens);
-                parser_setList(tokens->first);
-                SExpression *exp = parser_parseExpression();
-                lexer_clearTokenList(tokens);
-                parser_print(exp);
-                parser_clearExpression(exp);
-                
-                // Flush output stream for debugging
-                printf("\n");
-                fflush(stdout);
-            }
+            runLine(buffer, tokens, 0);
         }
 
         if (!feof(fptr)) perror("Error reading file");
@@ -49,34 +53,16 @@ int main(int argc, char *argv[]) {
     }
     else {
         printf("Welcome to the Lisp Interpreter REPL!\nUse Ctrl+C to exit\n");
-
+        
         while (1){
             printf("\n>");
 
             if (fgets(buffer, sizeof(buffer), stdin) != NULL){
-                // fgets includes the newline character if there's space.
-                // To remove it, you can find and replace it with a null terminator.
-                size_t len = strlen(buffer);
-                if (len > 0 && buffer[len-1] == '\n') {
-                    buffer[len-1] = '\0';
-                }
-
-                lexer_scanTokens(buffer, tokens);
-                if (tokens->size > 0){
-                    parser_setList(tokens->first);
-                    SExpression *exp = parser_parseExpression();
-                    lexer_clearTokenList(tokens);
-                    parser_print(exp);
-                    parser_clearExpression(exp);
-                }
+                runLine(buffer, tokens, 1);
             } 
             else {
                 fprintf(stderr, "Error reading input.\n");
             }
-
-            // Flush output stream for debugging
-            printf("\n");
-            fflush(stdout);
         }
     }
 
