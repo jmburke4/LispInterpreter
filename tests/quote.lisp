5dcedf91a185f1981ddd9803f1952887f15f7d73
ban <jmburke4@crimson.ua.edu>
2025-09-27 17:24:57 +0100

    Update quote.lisp

--- tests/quote.lisp
+++ tests/quote.lisp
@@ -1,4 +1,15 @@
-; Trying to test the distribution of quotes
-('a '(b c))
-('(a b) 'c)
-('(a b) '(c))
+(a b) ; -> (a b)
+(a b c) ; -> (a (b c))
+((a b) c) ; -> ((a b) c)
+(a (b c)) ; -> (a (b c))
+(a b c d) ; -> ((a b)(c d))
+
+('a b) ; -> ((quote a) b)
+(a 'b) ; -> (a (quote b))
+('a 'b) ; -> ((quote a) (quote b))
+
+(a '(b c)) ; -> (a (quote (b c)))
+('(a b) c) ; -> ((quote (a b)) c)
+
+('a '(b c)) ; -> ((quote a) (quote (b c)))
+('(a b) 'c) ; -> ((quote (a b)) (quote c))
